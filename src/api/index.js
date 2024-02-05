f093833726260a2b95f252ec8306a57838fdac62
Mauricio Cucco <mscucco@hotmail.com>
2024-02-05 09:17:10 -0300

    Uninstall dotenv

--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,8 @@
 import express from 'express'
-import dotenv from 'dotenv'
 
 import { mainMiddlewares } from './middlewares/main.middleware.js'
 import { routes } from './routes/main.route.js'
 import { errorMiddlewares } from './middlewares/errors.middleware.js'
-
-dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT ?? 3000
