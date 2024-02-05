82b3934f943b38f6d99159f7c6393b4d3ce0374d
Mauricio Cucco <mscucco@hotmail.com>
2024-02-05 14:59:47 -0300

    Add status relationship

--- src/api/models/postgre-dog.model.js
+++ src/api/models/postgre-dog.model.js
@@ -2,6 +2,13 @@ import { DataTypes } from 'sequelize'
 
 import { connection } from '../../config/postgre-database.config.js'
 
+export const Status = connection.define('Status', {
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+}, { timestamps: false, tableName: 'status' })
+
 export const Dog = connection.define('Dog', {
   name: {
     type: DataTypes.STRING,
@@ -9,7 +16,20 @@ export const Dog = connection.define('Dog', {
   },
   age: {
     type: DataTypes.INTEGER,
+    allowNull: true
+  },
+  sex: {
+    type: DataTypes.ENUM,
+    values: ['male', 'female'],
     allowNull: false
+  },
+  status_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  image: {
+    type: DataTypes.STRING,
+    allowNull: true
   },
   created_at: {
     type: DataTypes.DATE,
@@ -23,3 +43,6 @@ export const Dog = connection.define('Dog', {
     type: DataTypes.DATE
   }
 }, { timestamps: false, tableName: 'dogs' })
+
+Dog.belongsTo(Status, { foreignKey: 'status_id' })
+Status.hasMany(Dog, { foreignKey: 'status_id' })
