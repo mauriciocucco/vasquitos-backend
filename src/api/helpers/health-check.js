9a9dabb4369fffa136514a42c849ad72109017f5
Mauricio Cucco <mscucco@hotmail.com>
2024-04-07 15:36:41 -0300

    Remove cron

--- src/api/helpers/health-check.js
+++
@@ -1,8 +0,0 @@
-import cron from 'node-cron'
-
-export const healthCheck = () => {
-  // Schedule tasks to be run on the server.
-  cron.schedule('*/10 * * * *', function () {
-    console.log('Health check running every 10 minutes')
-  })
-}
--- src/api/index.js
+++ src/api/index.js
@@ -3,7 +3,6 @@ import express from 'express'
 import { mainMiddlewares } from './middlewares/main.middleware.js'
 import { routes } from './routes/main.route.js'
 import { errorsMiddlewares } from './middlewares/errors.middleware.js'
-import { healthCheck } from './helpers/health-check.js'
 
 const app = express()
 const PORT = process.env.PORT ?? 3000
@@ -19,7 +18,5 @@ errorsMiddlewares(app)
 app.listen(PORT, () => {
   console.log(`App is listening on port ${PORT}`)
 })
-
-healthCheck()
 
 export default app
